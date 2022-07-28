f3f03198e332ac8a2da9fc7ae77992b3775fbbd4
fazarhdyt <m.fajar184@gmail.com>
2022-07-28 14:01:09 +0700

    CRUD admin item

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -18,7 +18,7 @@ const storageMultiple = multer.diskStorage({
 
 const uploadMultiple = multer({
   storage: storageMultiple,
-  limits: { fileSize: 1000000 },
+  // limits: { fileSize: 1000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
@@ -33,13 +33,21 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({
+const uploadSingle = multer({
   storage: storage,
   limits: { fileSize: 1000000 },
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
 }).single("image");
+
+// const uploadMultiple = multer({
+//   storage: storage,
+//   limits: { fileSize: 1000000 },
+//   fileFilter: function (req, file, cb) {
+//     checkFileType(file, cb);
+//   }
+// }).array("image");
 
 // // Check file Type
 function checkFileType(file, cb) {
@@ -58,4 +66,4 @@ function checkFileType(file, cb) {
   }
 }
 
-module.exports = { uploadMultiple, upload };
+module.exports = { uploadMultiple, uploadSingle };
